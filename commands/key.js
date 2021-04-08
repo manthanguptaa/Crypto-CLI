962d1b99fe48235bce20efc03164eeb9e3b19008
Manthan Gupta <manthangupta109@gmail.com>
2021-04-08 19:52:12 +0530

    Fix: ES modules can't be imported using require

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,23 @@
+const KeyManager = require("../lib/KeyManager");
+const inquirer = require("inquirer");
+const colors = require("colors");
+
 const key = {
-  set() {
-    console.log("SET");
+  async set() {
+    const keyManager = new KeyManager();
+    const input = await inquirer.prompt([
+      {
+        type: "input",
+        name: "key",
+        message: "Enter API Key".green + " https://nomics.com",
+        validate: isRequired
+      },
+    ]);
+
+    const key = keyManager.setKey(input.key);
+    if (key) {
+      console.log("API Key Set".blue);
+    }
   },
   show() {
     console.log("SHOW");
